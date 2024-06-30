2eb18c7e27a131acd1aacc5a0f23c0f5bb4387fc
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2024-06-30 20:25:18 +0300

    refactor form

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -27,159 +27,141 @@ function Form() {
       <form
         action="#"
         method="POST"
-        className="flex flex-col place-content-center"
+        className="flex flex-col place-content-center text-center"
       >
-        <table>
-          <tr>
-            <th>
-              <label htmlFor="name" className="mx-4">
-                Full Name
-              </label>
-            </th>
-            <td className="w-1/2 text-left">
-              <input
-                id="name"
-                name="name"
-                type="text"
-                autoComplete="name"
-                required
-                className="border-my_dark rounded-lg shadow-sm p-1.5 w-full
-                focus:outline-none focus:border-accent focus:ring focus:ring-accent
-                text-2xl"
-              />
-            </td>
-          </tr>
+        <div className="flex flex-col items-center mb-4">
+          <label htmlFor="name" className="w-full">
+            Full Name
+          </label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            autoComplete="name"
+            required
+            className="border-my_dark rounded-lg shadow-sm p-1.5 w-1/2 hover:scale-110 duration-100
+            focus:outline-none focus:border-accent focus:ring focus:ring-accent
+            text-2xl"
+          />
+        </div>
 
-          <tr>
-            <th>
-              <label htmlFor="coming" className="mx-4">
-                Coming
+        <div className="flex flex-col items-center mb-4">
+          <label htmlFor="coming" className="w-full">
+            Coming
+          </label>
+          <div className="flex justify-center w-full">
+            <div className="me-4 hover:scale-125 duration-100">
+              <label htmlFor="comingYes" className="mr-2">
+                Yes
               </label>
-            </th>
-            <td className="w-1/2 text-left">
-              <div className="flex justify-center">
+              <input
+                id="comingYes"
+                name="coming"
+                type="radio"
+                value="yes"
+                checked={isComing === 'yes'}
+                onChange={handleComingChange}
+                className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
+                focus:outline-none focus:border-accent focus:ring focus:ring-accent"
+              />
+            </div>
+            <div className="hover:scale-125 duration-100">
+              <label htmlFor="comingNo" className="mr-2">
+                No
+              </label>
+              <input
+                id="comingNo"
+                name="coming"
+                type="radio"
+                value="no"
+                checked={isComing === 'no'}
+                onChange={handleComingChange}
+                className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
+                focus:outline-none focus:border-accent focus:ring focus:ring-accent"
+              />
+            </div>
+          </div>
+        </div>
+
+        {isComing === 'yes' && (
+          <>
+            <div className="flex flex-col items-center mb-4">
+              <label htmlFor="accompanied" className="w-full">
+                Accompanied
+              </label>
+              <div className="flex justify-center w-full">
                 <div className="me-4 hover:scale-125 duration-100">
-                  <label htmlFor="comingYes" className="mr-2">
+                  <label htmlFor="accompaniedYes" className="mr-2">
                     Yes
                   </label>
                   <input
-                    id="comingYes"
-                    name="coming"
+                    id="accompaniedYes"
+                    name="accompanied"
                     type="radio"
                     value="yes"
-                    checked={isComing === 'yes'}
-                    onChange={handleComingChange}
+                    checked={accompanied === 'yes'}
+                    onChange={handleAccompaniedChange}
                     className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
                     focus:outline-none focus:border-accent focus:ring focus:ring-accent"
                   />
                 </div>
                 <div className="hover:scale-125 duration-100">
-                  <label htmlFor="comingNo" className="mr-2">
+                  <label htmlFor="accompaniedNo" className="mr-2">
                     No
                   </label>
                   <input
-                    id="comingNo"
-                    name="coming"
+                    id="accompaniedNo"
+                    name="accompanied"
                     type="radio"
                     value="no"
-                    checked={isComing === 'no'}
-                    onChange={handleComingChange}
+                    checked={accompanied === 'no'}
+                    onChange={handleAccompaniedChange}
                     className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
                     focus:outline-none focus:border-accent focus:ring focus:ring-accent"
                   />
                 </div>
               </div>
-            </td>
-          </tr>
+            </div>
 
-          {isComing === 'yes' && (
-            <>
-              <tr>
-                <th>
-                  <label htmlFor="accompanied" className="mx-4">
-                    Accompanied
+            <div className="flex flex-col items-center mb-4">
+              <label htmlFor="menu" className="w-full">
+                Vegan menu
+              </label>
+              <div className="flex justify-center w-full">
+                <div className="me-4 hover:scale-125 duration-100">
+                  <label htmlFor="menuYes" className="mr-2">
+                    Yes
                   </label>
-                </th>
-                <td className="w-1/2 text-left">
-                  <div className="flex justify-center">
-                    <div className="me-4 hover:scale-125 duration-100">
-                      <label htmlFor="accompaniedYes" className="mr-2">
-                        Yes
-                      </label>
-                      <input
-                        id="accompaniedYes"
-                        name="accompanied"
-                        type="radio"
-                        value="yes"
-                        checked={accompanied === 'yes'}
-                        onChange={handleAccompaniedChange}
-                        className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
-                        focus:outline-none focus:border-accent focus:ring focus:ring-accent"
-                      />
-                    </div>
-                    <div className="hover:scale-125 duration-100">
-                      <label htmlFor="accompaniedNo" className="mr-2">
-                        No
-                      </label>
-                      <input
-                        id="accompaniedNo"
-                        name="accompanied"
-                        type="radio"
-                        value="no"
-                        checked={accompanied === 'no'}
-                        onChange={handleAccompaniedChange}
-                        className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
-                        focus:outline-none focus:border-accent focus:ring focus:ring-accent"
-                      />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-
-              <tr>
-                <th>
-                  <label htmlFor="menu" className="mx-4">
-                    Vegan menu
+                  <input
+                    id="menuYes"
+                    name="menu"
+                    type="radio"
+                    value="yes"
+                    checked={menu === 'yes'}
+                    onChange={handleMenuChange}
+                    className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
+                    focus:outline-none focus:border-accent focus:ring focus:ring-accent"
+                  />
+                </div>
+                <div className="hover:scale-125 duration-100">
+                  <label htmlFor="menuNo" className="mr-2">
+                    No
                   </label>
-                </th>
-                <td className="w-1/2 text-left">
-                  <div className="flex justify-center">
-                    <div className="me-4 hover:scale-125 duration-100">
-                      <label htmlFor="menuYes" className="mr-2">
-                        Yes
-                      </label>
-                      <input
-                        id="menuYes"
-                        name="menu"
-                        type="radio"
-                        value="yes"
-                        checked={menu === 'yes'}
-                        onChange={handleMenuChange}
-                        className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
-                        focus:outline-none focus:border-accent focus:ring focus:ring-accent"
-                      />
-                    </div>
-                    <div className="hover:scale-125 duration-100">
-                      <label htmlFor="menuNo" className="mr-2">
-                        No
-                      </label>
-                      <input
-                        id="menuNo"
-                        name="menu"
-                        type="radio"
-                        value="no"
-                        checked={menu === 'no'}
-                        onChange={handleMenuChange}
-                        className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
-                        focus:outline-none focus:border-accent focus:ring focus:ring-accent"
-                      />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </>
-          )}
-        </table>
+                  <input
+                    id="menuNo"
+                    name="menu"
+                    type="radio"
+                    value="no"
+                    checked={menu === 'no'}
+                    onChange={handleMenuChange}
+                    className="border-my_dark text-accent rounded-md w-6 h-6 shadow-sm 
+                    focus:outline-none focus:border-accent focus:ring focus:ring-accent"
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
 
         <div className="flex justify-center mt-6">
           <button
