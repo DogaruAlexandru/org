f82f9b4655c188ed3bffe8dd162321c8d90639c0
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2024-07-01 22:08:22 +0300

    added music but needs engagement

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import Invitaion from './components/invitaion';
 import Title from './components/title';
 import Slideshow from './components/images-slideshow';
@@ -6,14 +8,39 @@ import Form from './components/form';
 
 import bg_image from '../assets/images/bg.png';
 
+import music from '../assets/amazing-day-iros-young-main-version-18425-02-07.mp3';
+
 export function App() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.3;
+
+      // Function to play audio
+      const playAudio = () => {
+        audioRef.current?.play().catch((error) => {
+          console.error('Failed to play audio:', error);
+        });
+      };
+
+      // Add event listener for user interaction
+      document.addEventListener('click', playAudio);
+
+      // Clean up the event listener
+      return () => {
+        document.removeEventListener('click', playAudio);
+      };
+    }
+  }, []);
+
   return (
     <div
       style={{ backgroundImage: `url(${bg_image})` }}
       className="py-12 bg-repeat bg-bg px-32 space-y-6 
       scroll-smooth h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-accent-dark scrollbar-track-accent-light"
     >
-      {/* <NxWelcome title="org" /> */}
+      <audio ref={audioRef} src={music} autoPlay loop />
       <Title />
       <Slideshow />
       <Invitaion />
