b5ee89196d1cbc63f84a7d5ddef5e58d6c1ce817
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2024-07-01 20:45:15 +0300

    added background

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,11 +4,14 @@ import Slideshow from './components/images-slideshow';
 import Details from './components/details';
 import Form from './components/form';
 
+import bg_image from '../assets/images/bg.png';
+
 export function App() {
   return (
     <div
-      className="py-12 bg-bg px-32 space-y-4 scroll-smooth h-screen overflow-y-scroll 
-      scrollbar-thin scrollbar-thumb-accent-dark scrollbar-track-accent-light"
+      style={{ backgroundImage: `url(${bg_image})` }}
+      className="py-12 bg-repeat bg-bg px-32 space-y-6 
+      scroll-smooth h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-accent-dark scrollbar-track-accent-light"
     >
       {/* <NxWelcome title="org" /> */}
       <Title />
