c9586b0f25822ffacdee80a56b28e7b6d5d459f4
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2024-07-08 13:19:10 +0300

    notifications on save status

--- src/app/components/form/form.tsx
+++ src/app/components/form/form.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from 'react';
+
+import { writeValues } from '../../supabase';
+
 import RadioButtonGroup from './radio-button-group';
-import { fetchValuesById, writeValues } from '../../supabase';
+import { NotificationSelector, NotificationType } from '../notification';
+
+enum SaveStatus {
+  None,
+  SaveInProgress,
+  SaveSuccess,
+  SaveError,
+}
 
 interface FormProps {
   data: any;
@@ -13,6 +23,19 @@ const Form: React.FC<FormProps> = ({ data }) => {
   const [menu, setMenu] = useState(data.accompanied ? 'yes' : 'no');
   const [accompanied, setAccompanied] = useState(data.vegan ? 'yes' : 'no');
   const [extraMenu, setExtraMenu] = useState(data.extra_vegan ? 'yes' : 'no');
+
+  const [saveStatus, setSaveStatus] = useState(SaveStatus.None);
+
+  const setFormValues = () => {
+    setIsComing(data.coming ? 'yes' : 'no');
+    setMenu(data.accompanied ? 'yes' : 'no');
+    setAccompanied(data.vegan ? 'yes' : 'no');
+    setExtraMenu(data.extra_vegan ? 'yes' : 'no');
+  };
+
+  useEffect(() => {
+    setFormValues();
+  }, []);
 
   const handleComingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsComing(event.target.value);
@@ -36,104 +59,138 @@ const Form: React.FC<FormProps> = ({ data }) => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    const success = await writeValues(
-      id,
-      isComing === 'yes',
-      menu === 'yes',
-      accompanied === 'yes',
-      extraMenu === 'yes'
-    );
+    setSaveStatus(SaveStatus.SaveInProgress);
+    try {
+      const success = await writeValues(
+        id,
+        isComing === 'yes',
+        menu === 'yes',
+        accompanied === 'yes',
+        extraMenu === 'yes'
+      );
+      if (success) {
+        setSaveStatus(SaveStatus.SaveSuccess);
+        setTimeout(() => {
+          setSaveStatus(SaveStatus.None);
+        }, 2000);
+      } else {
+        setSaveStatus(SaveStatus.SaveError);
+        setFormValues();
+        setTimeout(() => {
+          setSaveStatus(SaveStatus.None);
+        }, 3000);
+      }
+    } catch (error) {
+      setSaveStatus(SaveStatus.SaveError);
+      setFormValues();
+      setTimeout(() => {
+        setSaveStatus(SaveStatus.None);
+      }, 3000);
+    }
   };
 
-  return (
-    <div
-      className="my-bg-band1 shadow-lg px-4 py-8 rounded-lg border border-my_dark 
-      font-dancing-script text-my_dark text-center font-bold text-3xl"
-    >
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col place-content-center text-center"
-      >
-        <div className="flex flex-col items-center mb-4">
-          <label htmlFor="name" className="w-full">
-            Full Name
-          </label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            autoComplete="name"
-            required
-            disabled={true}
-            value={name}
-            className="border-my_dark rounded-lg shadow-sm p-1.5 w-1/2 hover:scale-110 duration-100
-            focus:outline-none focus:border-accent focus:ring focus:ring-accent
-            text-2xl"
-          />
-        </div>
+  return (() => {
+    switch (saveStatus) {
+      case SaveStatus.None:
+        return (
+          <div
+            className="my-bg-band1 shadow-lg px-4 py-8 rounded-lg border border-my_dark 
+          font-dancing-script text-my_dark text-center font-bold text-3xl"
+          >
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col place-content-center text-center"
+            >
+              <div className="flex flex-col items-center mb-4">
+                <label htmlFor="name" className="w-full">
+                  Full Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  disabled={true}
+                  value={name}
+                  className="border-my_dark rounded-lg shadow-sm p-1.5 w-1/2 hover:scale-110 duration-100
+                focus:outline-none focus:border-accent focus:ring focus:ring-accent
+                text-2xl"
+                />
+              </div>
 
-        <RadioButtonGroup
-          idPrefix="coming"
-          label="Coming"
-          options={[
-            { label: 'Yes', value: 'yes' },
-            { label: 'No', value: 'no' },
-          ]}
-          selectedValue={isComing}
-          onChange={handleComingChange}
-        />
-
-        {isComing === 'yes' && (
-          <>
-            <RadioButtonGroup
-              idPrefix="menu"
-              label="Vegan menu"
-              options={[
-                { label: 'Yes', value: 'yes' },
-                { label: 'No', value: 'no' },
-              ]}
-              selectedValue={menu}
-              onChange={handleMenuChange}
-            />
-
-            <RadioButtonGroup
-              idPrefix="accompanied"
-              label="Accompanied"
-              options={[
-                { label: 'Yes', value: 'yes' },
-                { label: 'No', value: 'no' },
-              ]}
-              selectedValue={accompanied}
-              onChange={handleAccompaniedChange}
-            />
-
-            {accompanied === 'yes' && (
               <RadioButtonGroup
-                idPrefix="extraMenu"
-                label="Vegan menu for your guest"
+                idPrefix="coming"
+                label="Coming"
                 options={[
                   { label: 'Yes', value: 'yes' },
                   { label: 'No', value: 'no' },
                 ]}
-                selectedValue={extraMenu}
-                onChange={handleExtraMenuChange}
+                selectedValue={isComing}
+                onChange={handleComingChange}
               />
-            )}
-          </>
-        )}
 
-        <div className="flex justify-center mt-6">
-          <button
-            type="submit"
-            className="my-bg-band2 px-10 py-1 rounded-xl border border-my_dark shadow-lg
-            hover-bg-band2 hover:text-white hover:scale-110 duration-100"
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+              {isComing === 'yes' && (
+                <>
+                  <RadioButtonGroup
+                    idPrefix="menu"
+                    label="Vegan menu"
+                    options={[
+                      { label: 'Yes', value: 'yes' },
+                      { label: 'No', value: 'no' },
+                    ]}
+                    selectedValue={menu}
+                    onChange={handleMenuChange}
+                  />
+
+                  <RadioButtonGroup
+                    idPrefix="accompanied"
+                    label="Accompanied"
+                    options={[
+                      { label: 'Yes', value: 'yes' },
+                      { label: 'No', value: 'no' },
+                    ]}
+                    selectedValue={accompanied}
+                    onChange={handleAccompaniedChange}
+                  />
+
+                  {accompanied === 'yes' && (
+                    <RadioButtonGroup
+                      idPrefix="extraMenu"
+                      label="Vegan menu for your guest"
+                      options={[
+                        { label: 'Yes', value: 'yes' },
+                        { label: 'No', value: 'no' },
+                      ]}
+                      selectedValue={extraMenu}
+                      onChange={handleExtraMenuChange}
+                    />
+                  )}
+                </>
+              )}
+
+              <div className="flex justify-center mt-6">
+                <button
+                  type="submit"
+                  className="my-bg-band2 px-10 py-1 rounded-xl border border-my_dark shadow-lg
+                hover-bg-band2 hover:text-white hover:scale-110 duration-100"
+                >
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        );
+      case SaveStatus.SaveInProgress:
+        return <NotificationSelector type={NotificationType.SaveInProgress} />;
+      case SaveStatus.SaveSuccess:
+        return <NotificationSelector type={NotificationType.SaveSuccess} />;
+      case SaveStatus.SaveError:
+        return <NotificationSelector type={NotificationType.SaveError} />;
+      default:
+        return null;
+    }
+  })();
 };
 
 export default Form;
