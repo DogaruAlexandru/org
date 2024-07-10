d6ee3c4d714a6b37e30deb0f77afd61d51f91ec7
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2024-07-10 18:01:38 +0300

    Update time-left.tsx

--- src/app/components/form/time-left.tsx
+++ src/app/components/form/time-left.tsx
@@ -1,4 +1,3 @@
-import { set } from 'date-fns';
 import { useState, useEffect } from 'react';
 
 interface TimeLeftProps {
@@ -14,19 +13,12 @@ const TimeLeft: React.FC<TimeLeftProps> = ({ setCanModify }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       const currentDateTime = new Date(
-        new Intl.DateTimeFormat('en-US', {
-          timeZone: 'Europe/Bucharest',
-          hour12: false,
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-        }).format(new Date())
+        new Date().toLocaleString('en-US', { timeZone: 'Europe/Bucharest' })
       );
 
-      if (currentDateTime > deadline) {
+      const timeDiff = deadline.getTime() - currentDateTime.getTime();
+
+      if (timeDiff <= 0) {
         setBgColor('my-red-grd');
         setTextColor('text-white');
         setCanModify(false);
@@ -35,7 +27,17 @@ const TimeLeft: React.FC<TimeLeftProps> = ({ setCanModify }) => {
         setBgColor('my-bg-band3');
         setTextColor('text-my_dark');
         setCanModify(true);
-        setText(`Modifiable until: ${deadline.toLocaleString('ro-RO')}`);
+
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((timeDiff / (1000 * 60)) % 60);
+        const seconds = Math.floor((timeDiff / 1000) % 60);
+
+        setText(
+          `Modifiable until: ${deadline.toLocaleString(
+            'ro-RO'
+          )} (${days}d ${hours}h ${minutes}m ${seconds}s left)`
+        );
       }
     }, 1000);
 
