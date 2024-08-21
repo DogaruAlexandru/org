dd4a8de08f978e33c85e1c3b797b4875b7f9dab0
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2024-08-21 20:56:07 +0300

    changed limit time

--- src/app/components/form/time-left.tsx
+++ src/app/components/form/time-left.tsx
@@ -5,7 +5,7 @@ interface TimeLeftProps {
 }
 
 const TimeLeft: React.FC<TimeLeftProps> = ({ setCanModify }) => {
-  const deadline = new Date('2024-09-01T00:00:00+03:00'); // New deadline date in Bucharest timezone
+  const deadline = new Date('2025-01-02T00:00:00+03:00'); // New deadline date in Bucharest timezone
   const deadlineText = `Modificabil până la: ${deadline.toLocaleDateString(
     'ro-RO'
   )}`;
